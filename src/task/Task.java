44d8364000104d663a251191389a456ebd3dee66
Mongol666 <pisarevgerman78@gmail.com>
2021-09-18 16:11:36 +0300

    constructors are added

--- src/task/Task.java
+++ src/task/Task.java
@@ -3,6 +3,17 @@ package task;
 public class Task {
     private int first_number;
     private int second_number;
-    
+
+    public Task() {
+    }
+
+    public Task(int first_number) {
+        this.first_number = first_number;
+    }
+
+    public Task(int first_number, int second_number) {
+        this(first_number);
+        this.second_number = second_number;
+    }
 
 }
